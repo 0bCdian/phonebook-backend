677a0251c344459f9584fe3ad94bb96e735f6883
0bCdian <diegomiguelp1996@gmail.com>
2023-02-20 09:34:40 -0300

    3.8*: Phonebook backend step8

--- index.js
+++ index.js
@@ -3,8 +3,21 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const PORT = '3001'
+
+morgan.token('data', (req) => {
+  const data = req.body
+  const dataLength = Object.keys(data).length
+  if (dataLength > 0) {
+    return JSON.stringify(req.body)
+  } else {
+    return ' '
+  }
+})
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :data')
+)
 
 // ! Data
 // eslint-disable-next-line prefer-const
