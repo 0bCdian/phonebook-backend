8781daa46b13438b97a024023a4640a777a9609f
0bCdian <diegomiguelp1996@gmail.com>
2023-03-01 09:31:38 -0300

    Delete unused mongo.js file

--- mongo.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
-mongoose.set('strictQuery', true)
-const connectionString = process.env.MONGO_URI
-// conect to mongodb
-mongoose.connect(connectionString).catch((err) => console.error(err))
